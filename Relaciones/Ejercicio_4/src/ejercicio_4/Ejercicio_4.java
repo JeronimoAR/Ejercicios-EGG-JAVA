eb11dac74339e7e58bb76544b29ea8f6af86d4dd
jero <jeronimoarias1212@gmail.com>
2022-11-10 18:44:31 -0500

    22

---
+++ Relaciones/Ejercicio_4/src/ejercicio_4/Ejercicio_4.java
@@ -0,0 +1,13 @@
+package ejercicio_4;
+
+import ejercicio_4.Service.Servicio;
+
+public class Ejercicio_4 {
+
+    public static void main(String[] args) {
+        Servicio obj = new Servicio();
+        obj.crearPeliculas();
+        obj.mostrarPeliculas();
+    }
+    
+}
